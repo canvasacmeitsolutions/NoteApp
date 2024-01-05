30572b240695491d0f3ebd96c4e267c0b98bf961
acmesurajpagare <102643837+acmesurajpagare@users.noreply.github.com>
2024-01-05 11:55:06 +0530

    Update NoteDatabase.kt

--- app/src/main/java/com/fabfinance/noteapp/services/NoteDatabase.kt
+++ app/src/main/java/com/fabfinance/noteapp/services/NoteDatabase.kt
@@ -3,7 +3,6 @@ package com.fabfinance.noteapp.services
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import com.fabfinance.noteapp.model.Note
 import kotlinx.coroutines.internal.synchronized
 
